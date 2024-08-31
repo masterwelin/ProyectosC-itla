017562a5daac4e8f7d871d7ce191e980a68db727
masterwelin <masterwelin@gmail.com>
2024-08-30 23:26:24 -0400

    arreglo menu

--- PracticaArreglos/Program.cs
+++ PracticaArreglos/Program.cs
@@ -1,5 +1,41 @@
 ﻿using PracticaArreglos.Practicas;
 
+string linea = string.Empty;
+int opcion = 0;
+
+Console.WriteLine("Elija cual programa quiere ejecutar:\n1)Programa Array 1\n2)Programa Array 2\n3)Programa Array 3");
+
+linea = Console.ReadLine();
+
+if (!int.TryParse(linea, out opcion))
+{
+    Console.WriteLine("La opción elegida es invalida es invalida.");
+    return;
+}
+
+switch (opcion)
+
+{
+    case 1:
+        ProgramaArray1 programaArray1 = new ProgramaArray1();
+        programaArray1.Calcular();
+        break;
+
+    case 2:
+        ProgramaArray2 programaArray2 = new ProgramaArray2();
+        programaArray2.Calcular();
+        break;
+
+    case 3:
+        ProgramaArray3 programaArray3 = new ProgramaArray3();
+        programaArray3.Calcular();
+        break;
+
+    default:
+        Console.WriteLine("Opción elegida es invalida...");
+        break;
+}
+
 /*
 ProgramaArray1 programaArray1 = new ProgramaArray1();
 programaArray1.Calcular();
@@ -8,5 +44,5 @@ programaArray1.Calcular();
 /*ProgramaArray2 programaArray2 = new ProgramaArray2();
 programaArray2.Calcular();*/
 
-ProgramaArray3 programaArray3 = new ProgramaArray3();
-programaArray3.Calcular();
+/*ProgramaArray3 programaArray3 = new ProgramaArray3();
+programaArray3.Calcular();*/
