226a0a4b65da347933bd3be675c67bf6ac5100e1
masterwelin <masterwelin@gmail.com>
2024-09-04 17:57:41 -0400

    arreglo

---
+++ EjerciciosResumenLibro/Fig85InicArreglo.cs
@@ -0,0 +1,22 @@
+﻿
+namespace EjerciciosResumenLibro
+{
+    internal class Fig85InicArreglo
+    {
+        // Fig. 8.5: SumaArreglo.cs
+        // Cálculo de la suma de los elementos de un arreglo.
+
+        public static void Calcular()
+        {
+            int[] arreglo = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+            int total = 0;
+
+            // sumar el valor de cada elemento al total
+            for (int contador = 0; contador < arreglo.Length; contador++)
+                total += arreglo[contador];
+
+            Console.WriteLine("Total de los elementos del arreglo: {0}", total);
+        } // fin
+    } // fin de la clase Fig85InicArreglo
+
+}
--- EjerciciosResumenLibro/Program.cs
+++ EjerciciosResumenLibro/Program.cs
@@ -5,5 +5,17 @@
 //InicArreglo.Calcular();
 
 //Ejercicio 2
-Fig83InicArreglo fig83InicArreglo = new Fig83InicArreglo();
-Fig83InicArreglo.Calcular();
+//Fig83InicArreglo fig83InicArreglo = new Fig83InicArreglo();
+//Fig83InicArreglo.Calcular();
+
+//Ejercicio 3
+//Fig84InicArreglo fig84InicArreglo = new Fig84InicArreglo();
+//Fig84InicArreglo.Calcular();
+
+//Ejercicio 4
+//Fig85InicArreglo fig85InicArreglo = new Fig85InicArreglo();
+//Fig85InicArreglo.Calcular();
+
+//Ejercicio 5
+Fig86InicArreglo fig86InicArreglo = new Fig86InicArreglo();
+Fig86InicArreglo.Calcular();
