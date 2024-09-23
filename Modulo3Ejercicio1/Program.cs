83cb229e8f90fa4e126e11a08f2811fd7ecb6fb6
masterwelin <masterwelin@gmail.com>
2024-09-22 23:20:31 -0400

    ejercicio 1 modulo 3

---
+++ Modulo3Ejercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Modulo3Ejercicio1;
+
+
+Punto punto1 = new Punto();
+
+// Cargar valores de X e Y
+punto1.CargarValores(4, 5);
+punto1.ImprimirCuadrante();  
+
+Punto punto2 = new Punto();
+punto2.CargarValores(-3, 6);
+punto2.ImprimirCuadrante();  
+
+Punto punto3 = new Punto();
+punto3.CargarValores(0, -7);
+punto3.ImprimirCuadrante();  
+
+Console.ReadLine();
---
+++ Modulo3Ejercicio1/Punto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo3Ejercicio1
+{
+    public class Punto
+    {
+        // Propiedades para las coordenadas del punto
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        // Método para setear los valores
+        public void CargarValores(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Aqui determinamos en que cuadrante se encuentra el punto.
+        public void ImprimirCuadrante()
+        {
+            if (X > 0 && Y > 0)
+            {
+                Console.WriteLine("El punto se encuentra en el primer cuadrante.");
+            }
+            else if (X < 0 && Y > 0)
+            {
+                Console.WriteLine("El punto se encuentra en el segundo cuadrante.");
+            }
+            else if (X < 0 && Y < 0)
+            {
+                Console.WriteLine("El punto se encuentra en el tercer cuadrante.");
+            }
+            else if (X > 0 && Y < 0)
+            {
+                Console.WriteLine("El punto se encuentra en el cuarto cuadrante.");
+            }
+            else if (X == 0 && Y == 0)
+            {
+                Console.WriteLine("El punto se encuentra en el origen.");
+            }
+            else if (X == 0)
+            {
+                Console.WriteLine("El punto se encuentra sobre el eje Y.");
+            }
+            else if (Y == 0)
+            {
+                Console.WriteLine("El punto se encuentra sobre el eje X.");
+            }
+        }
+
+    }
+
+
+
+}
