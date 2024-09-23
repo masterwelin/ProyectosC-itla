c5f830525fe6611eb9d11646edfeb57bf26cc934
masterwelin <masterwelin@gmail.com>
2024-09-22 21:48:32 -0400

    practica modulo 3

---
+++ PracticasModulo3/Ejercicio2/Libro.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PracticasModulo3.Ejercicio2
+{
+    /// <summary>
+    /// Ejercicio 2
+    ///Crea una clase Libro con las propiedades Titulo, Autor y Paginas.
+    ///Implementa un método MostrarInformacion que imprima la información del libro.
+    ///Implementa un método EsLargo que devuelva true si el libro tiene más de 500 páginas, 
+    ///o false en caso contrario.
+    /// </summary>
+    public class Libro
+    {
+
+        public string titulo;
+        public string autor;
+        public int paginas;
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+
+        public int Paginas
+        {
+            get { return paginas; }
+            set { paginas = value; }
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"el libro tiene las siguientes informaciones:\nTitulo del libro: {Titulo}\nAutor:{Autor}\nCantidad de paginas:{Paginas}");
+        }
+
+        public bool EsLargo()
+        {
+
+            if (Paginas >= 500)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}
---
+++ PracticasModulo3/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using PracticasModulo3.Ejercicio2;
+using System.Timers;
+
+Libro libro = new Libro();
+
+// Seteando las propiedades
+Console.WriteLine("Inserte el autor del libro que sera pasado a la propiedad Autor de la clase libro...");
+
+try
+{
+    libro.autor = Console.ReadLine();
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"ha ocurrido el siguiente error {ex}");
+}
+Console.WriteLine("Inserte el titulo del libro que sera pasado a la propiedad Titulo de la clase libro...");
+
+try
+{
+    libro.titulo = Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ha ocurrido el siguiente error {ex}");
+}
+
+Console.WriteLine("Inserte la cantidad de paginas que sera pasada a la propiedad Paginas de la clase libro...");
+
+try
+{
+    libro.paginas = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ha ocurrido el siguiente error {ex}");
+}
+
+libro.MostrarInformacion();
+
+if (libro.EsLargo() == true)
+{
+    Console.WriteLine("El metodo EsLargo devolvio true por lo que el libro es laargo");
+}
+else 
+{
+    Console.WriteLine("El libro no es largo");
+}
+
+Console.ReadLine();
